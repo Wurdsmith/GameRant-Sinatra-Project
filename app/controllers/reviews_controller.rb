e5049d1770fde230df9a0024f0184d5088690fb3
Wurdsmith <wurdsmithing@gmail.com>
2021-03-05 20:29:30 -0600

    Adds dynamic /review/new routes on the /games page that correspond to each game id.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,10 @@ class ReviewsController < ApplicationController
         erb :'reviews/index'
       end
     
-      get '/reviews/new' do
+      get '/reviews/:id/new' do
         redirect_if_not_logged_in
+        game_hash = Game.find_by(id: params[:id])
+        @game_name = game_hash.name
         erb :'reviews/new'
       end
 
